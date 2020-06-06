307358df01ace3e7539c963a4a88f1594ea463cc
Raunak Jalan <ronakjalan98@gmail.com>
2020-06-06 21:04:02 +0530

    Assignment 2: Angular Services, Routing and Single Page Applications

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Leader } from from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+	
+	leaders: Leader[];
+	
+  	constructor(private leaderService: LeaderService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  	ngOnInit() {
+		this.leaders = this.leaderService.getLeaders();
+  	}
 
 }
