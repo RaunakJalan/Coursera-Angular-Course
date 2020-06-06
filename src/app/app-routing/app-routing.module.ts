4bb84c744bfd9d941b353f8260bfee9dfb6f0218
Raunak Jalan <ronakjalan98@gmail.com>
2020-06-06 14:04:48 +0530

    Added Angular Routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { routes } from './routes'
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+	RouterModule.forRoot(routes)
+  ],
+  exports: [
+	  RouterModule
   ],
   declarations: []
 })
---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { AboutComponent } from '../about/about.component';
+import { HomeComponent } from '../home/home.component';
+import { ContactComponent } from '../contact/contact.component';
+
+
+export const routes: Routes = [
+	{
+		path: 'home',
+		component: HomeComponent
+	},
+	{
+		path: 'menu',
+		component: MenuComponent
+	},
+	{
+		path: 'about',
+		component: AboutComponent
+	},
+	{
+		path: 'contact',
+		component: ContactComponent
+	},
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	}
+];
+
