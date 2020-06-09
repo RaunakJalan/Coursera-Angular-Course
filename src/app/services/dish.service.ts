21ccbc823902340a37a30dd71d59a3c2a90b82cf
Raunak Jalan <ronakjalan98@gmail.com>
2020-06-09 19:45:20 +0530

    Added Promise with Delay

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,14 +11,26 @@ export class DishService {
   constructor() { }
 	
   getDishes(): Promise<Dish[]> {
-	  return Promise.resolve(DISHES);
+	  //return Promise.resolve(DISHES);
+	  return new Promise(resolve => {
+		  //Stimulate server latency with 2 sec delay
+		  setTimeout(() => resolve(DISHES), 2000);
+	  });
   }
   
   getDish(id: string): Promise<Dish> {
-	  return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+	  //return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]
+	  return new Promise(resolve => {
+		  //Stimulate server latency with 2 sec delay
+		  setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+	  });
   }
 	
   getFeaturedDish(): Promise<Dish> {
-	  return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+	  //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+	  return new Promise(resolve => {
+		  //Stimulate server latency with 2 sec delay
+		  setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+	  });
   }
 }
