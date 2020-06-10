d065255e7fcc0d76bd3562e62d17fe8cbd3ed4ab
Raunak Jalan <ronakjalan98@gmail.com>
2020-06-10 17:15:50 +0530

    Added JSON Server HTTP response to Promotions

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
-import { PROMOTIONS } from '../shared/promotions';
+//import { PROMOTIONS } from '../shared/promotions';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() { }
+  constructor(private  http: HttpClient) { }
   
   getPromotions(): Observable<Promotion[]> {
 	  //return Promise.resolve(PROMOTIONS);;
-	  return of(PROMOTIONS).pipe(delay(2000));
+	  return this.http.get<Promotion[]>(baseURL + 'promotions');
   }
   
   getPromotion(id: string): Observable<Promotion> {
 	  //return Promise.resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]);
-	  return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+	  return this.http.get<Promotion>(baseURL + 'promotions/' + id);
  }
 	
   getFeaturedPromotion(): Observable<Promotion> {
 	  //return Promise.resolve(PROMOTIONS.filter((promo) => promo.featured)[0]);
-	  return of(PROMOTIONS.filter((lead) => lead.featured)[0]).pipe(delay(2000));
+	  return this.http.get<Promotion[]>(baseURL + 'promotions?featured=true').pipe(map(promotion => promotion[0]));
   }
 	  
 }
